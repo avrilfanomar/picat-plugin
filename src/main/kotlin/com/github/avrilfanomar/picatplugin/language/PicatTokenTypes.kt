7ca0c6d052e35749bcc7e9c454e4dbdf86b5f48a
Andrii Andriichuk <andrey000mar@gmail.com>
2025-05-13 00:50:22 +0200

    update lexer and tokens

--- src/main/kotlin/com/github/avrilfanomar/picatplugin/language/PicatTokenTypes.kt
+++ src/main/kotlin/com/github/avrilfanomar/picatplugin/language/PicatTokenTypes.kt
@@ -13,6 +13,7 @@ object PicatTokenTypes {
     val INDEX_KEYWORD = PicatTokenType("INDEX_KEYWORD")
     val PRIVATE_KEYWORD = PicatTokenType("PRIVATE_KEYWORD")
     val PUBLIC_KEYWORD = PicatTokenType("PUBLIC_KEYWORD")
+    val TABLE_KEYWORD = PicatTokenType("TABLE_KEYWORD")
     val END_KEYWORD = PicatTokenType("END_KEYWORD")
     val IF_KEYWORD = PicatTokenType("IF_KEYWORD")
     val THEN_KEYWORD = PicatTokenType("THEN_KEYWORD")
@@ -26,6 +27,13 @@ object PicatTokenTypes {
     val THROW_KEYWORD = PicatTokenType("THROW_KEYWORD")
     val TRY_KEYWORD = PicatTokenType("TRY_KEYWORD")
     val CATCH_KEYWORD = PicatTokenType("CATCH_KEYWORD")
+    val NOT_KEYWORD = PicatTokenType("NOT_KEYWORD")
+    val ONCE_KEYWORD = PicatTokenType("ONCE_KEYWORD")
+    val DIV_KEYWORD = PicatTokenType("DIV_KEYWORD")
+    val MOD_KEYWORD = PicatTokenType("MOD_KEYWORD")
+    val REM_KEYWORD = PicatTokenType("REM_KEYWORD")
+    val IN_KEYWORD = PicatTokenType("IN_KEYWORD")
+    val NOTIN_KEYWORD = PicatTokenType("NOTIN_KEYWORD")
 
     // Operators
     val ASSIGN_OP = PicatTokenType("ASSIGN_OP") // =
@@ -34,12 +42,19 @@ object PicatTokenTypes {
     val PLUS = PicatTokenType("PLUS") // +
     val MINUS = PicatTokenType("MINUS") // -
     val MULTIPLY = PicatTokenType("MULTIPLY") // *
+    val POWER_OP = PicatTokenType("POWER_OP") // **
     val DIVIDE = PicatTokenType("DIVIDE") // /
+    val INT_DIVIDE = PicatTokenType("INT_DIVIDE") // //
+    val DIVIDE_LT = PicatTokenType("DIVIDE_LT") // /<
+    val DIVIDE_GT = PicatTokenType("DIVIDE_GT") // />
     val MODULO = PicatTokenType("MODULO") // mod
     val EQUAL = PicatTokenType("EQUAL") // ==
     val NOT_EQUAL = PicatTokenType("NOT_EQUAL") // !=
+    val NOT_IDENTICAL = PicatTokenType("NOT_IDENTICAL") // !==
+    val IDENTICAL = PicatTokenType("IDENTICAL") // ===
     val LESS = PicatTokenType("LESS") // <
     val LESS_EQUAL = PicatTokenType("LESS_EQUAL") // <=
+    val LESS_EQUAL_ALT = PicatTokenType("LESS_EQUAL_ALT") // =<
     val GREATER = PicatTokenType("GREATER") // >
     val GREATER_EQUAL = PicatTokenType("GREATER_EQUAL") // >=
     val AND = PicatTokenType("AND") // &&
@@ -52,6 +67,36 @@ object PicatTokenTypes {
     val TILDE = PicatTokenType("TILDE") // ~
     val BACKSLASH = PicatTokenType("BACKSLASH") // \
     val NOT = PicatTokenType("NOT") // !
+    val SHIFT_LEFT = PicatTokenType("SHIFT_LEFT") // <<
+    val SHIFT_RIGHT = PicatTokenType("SHIFT_RIGHT") // >>
+    val BITWISE_AND = PicatTokenType("BITWISE_AND") // /\
+    val BITWISE_OR = PicatTokenType("BITWISE_OR") // \/
+    val RANGE = PicatTokenType("RANGE") // ..
+    val CONCAT = PicatTokenType("CONCAT") // ++
+    val TYPE_CONSTRAINT = PicatTokenType("TYPE_CONSTRAINT") // ::
+    val ASSIGN_ONCE = PicatTokenType("ASSIGN_ONCE") // :=
+
+    // Constraint operators
+    val CONSTRAINT_EQ = PicatTokenType("CONSTRAINT_EQ") // #=
+    val CONSTRAINT_NEQ = PicatTokenType("CONSTRAINT_NEQ") // #!=
+    val CONSTRAINT_LT = PicatTokenType("CONSTRAINT_LT") // #<
+    val CONSTRAINT_LE = PicatTokenType("CONSTRAINT_LE") // #=<
+    val CONSTRAINT_LE_ALT = PicatTokenType("CONSTRAINT_LE_ALT") // #<=
+    val CONSTRAINT_GT = PicatTokenType("CONSTRAINT_GT") // #>
+    val CONSTRAINT_GE = PicatTokenType("CONSTRAINT_GE") // #>=
+    val CONSTRAINT_NOT = PicatTokenType("CONSTRAINT_NOT") // #~
+    val CONSTRAINT_AND = PicatTokenType("CONSTRAINT_AND") // #/\
+    val CONSTRAINT_OR = PicatTokenType("CONSTRAINT_OR") // #\/
+    val CONSTRAINT_XOR = PicatTokenType("CONSTRAINT_XOR") // #^
+    val CONSTRAINT_IMPL = PicatTokenType("CONSTRAINT_IMPL") // #=>
+    val CONSTRAINT_EQUIV = PicatTokenType("CONSTRAINT_EQUIV") // #<=>
+
+    // Term comparison operators
+    val TERM_LT = PicatTokenType("TERM_LT") // @<
+    val TERM_LE = PicatTokenType("TERM_LE") // @=<
+    val TERM_LE_ALT = PicatTokenType("TERM_LE_ALT") // @<=
+    val TERM_GT = PicatTokenType("TERM_GT") // @>
+    val TERM_GE = PicatTokenType("TERM_GE") // @>=
 
     // Separators
     val COMMA = PicatTokenType("COMMA") // ,
@@ -67,10 +112,15 @@ object PicatTokenTypes {
 
     // Literals
     val INTEGER = PicatTokenType("INTEGER")
+    val HEX_INTEGER = PicatTokenType("HEX_INTEGER")
+    val OCTAL_INTEGER = PicatTokenType("OCTAL_INTEGER")
+    val BINARY_INTEGER = PicatTokenType("BINARY_INTEGER")
     val FLOAT = PicatTokenType("FLOAT")
     val STRING = PicatTokenType("STRING")
     val ATOM = PicatTokenType("ATOM")
+    val QUOTED_ATOM = PicatTokenType("QUOTED_ATOM")
     val VARIABLE = PicatTokenType("VARIABLE")
+    val ANONYMOUS_VARIABLE = PicatTokenType("ANONYMOUS_VARIABLE")
 
     // Comments
     val COMMENT = PicatTokenType("COMMENT")
@@ -82,20 +132,30 @@ object PicatTokenTypes {
 
     // Token sets
     val KEYWORDS = TokenSet.create(
-        IMPORT_KEYWORD, MODULE_KEYWORD, INDEX_KEYWORD, PRIVATE_KEYWORD, PUBLIC_KEYWORD,
+        IMPORT_KEYWORD, MODULE_KEYWORD, INDEX_KEYWORD, PRIVATE_KEYWORD, PUBLIC_KEYWORD, TABLE_KEYWORD,
         END_KEYWORD, IF_KEYWORD, THEN_KEYWORD, ELSE_KEYWORD, ELSEIF_KEYWORD,
         WHILE_KEYWORD, DO_KEYWORD, FOREACH_KEYWORD, FOR_KEYWORD, RETURN_KEYWORD,
-        THROW_KEYWORD, TRY_KEYWORD, CATCH_KEYWORD
+        THROW_KEYWORD, TRY_KEYWORD, CATCH_KEYWORD, NOT_KEYWORD, ONCE_KEYWORD,
+        DIV_KEYWORD, MOD_KEYWORD, REM_KEYWORD, IN_KEYWORD, NOTIN_KEYWORD
     )
 
     val OPERATORS = TokenSet.create(
-        ASSIGN_OP, ARROW_OP, BACKTRACKABLE_ARROW_OP, PLUS, MINUS, MULTIPLY, DIVIDE, MODULO,
-        EQUAL, NOT_EQUAL, LESS, LESS_EQUAL, GREATER, GREATER_EQUAL,
-        AND, OR, PIPE, AS_PATTERN, DATA_CONSTRUCTOR, POWER, HASH, TILDE, BACKSLASH, NOT
+        ASSIGN_OP, ARROW_OP, BACKTRACKABLE_ARROW_OP, PLUS, MINUS, MULTIPLY, POWER_OP, 
+        DIVIDE, INT_DIVIDE, DIVIDE_LT, DIVIDE_GT, MODULO,
+        EQUAL, NOT_EQUAL, NOT_IDENTICAL, IDENTICAL, LESS, LESS_EQUAL, LESS_EQUAL_ALT, 
+        GREATER, GREATER_EQUAL, AND, OR, PIPE, AS_PATTERN, DATA_CONSTRUCTOR, 
+        POWER, HASH, TILDE, BACKSLASH, NOT, SHIFT_LEFT, SHIFT_RIGHT,
+        BITWISE_AND, BITWISE_OR, RANGE, CONCAT, TYPE_CONSTRAINT, ASSIGN_ONCE,
+        CONSTRAINT_EQ, CONSTRAINT_NEQ, CONSTRAINT_LT, CONSTRAINT_LE, CONSTRAINT_LE_ALT,
+        CONSTRAINT_GT, CONSTRAINT_GE, CONSTRAINT_NOT, CONSTRAINT_AND, CONSTRAINT_OR,
+        CONSTRAINT_XOR, CONSTRAINT_IMPL, CONSTRAINT_EQUIV,
+        TERM_LT, TERM_LE, TERM_LE_ALT, TERM_GT, TERM_GE
     )
 
     val COMMENTS = TokenSet.create(COMMENT)
     val STRING_LITERALS = TokenSet.create(STRING)
-    val NUMBERS = TokenSet.create(INTEGER, FLOAT)
+    val NUMBERS = TokenSet.create(INTEGER, HEX_INTEGER, OCTAL_INTEGER, BINARY_INTEGER, FLOAT)
+    val ATOMS = TokenSet.create(ATOM, QUOTED_ATOM)
+    val VARIABLES = TokenSet.create(VARIABLE, ANONYMOUS_VARIABLE)
     val WHITESPACES = TokenSet.create(WHITE_SPACE)
 }
--- src/main/kotlin/com/github/avrilfanomar/picatplugin/language/lexer/PicatLexer.kt
+++ src/main/kotlin/com/github/avrilfanomar/picatplugin/language/lexer/PicatLexer.kt
@@ -76,8 +76,36 @@ class PicatLexer : LexerBase() {
             return
         }
 
+        // Handle single-quoted atoms
+        if (buffer[tokenStart] == '\'') {
+            tokenEnd = skipQuotedAtom(tokenStart)
+            currentToken = PicatTokenTypes.QUOTED_ATOM
+            return
+        }
+
         // Handle numbers
         if (isDigit(buffer[tokenStart])) {
+            // Check for hex, octal, or binary numbers
+            if (tokenStart + 2 < bufferEnd && buffer[tokenStart] == '0') {
+                when (buffer[tokenStart + 1]) {
+                    'x', 'X' -> {
+                        tokenEnd = skipHexNumber(tokenStart)
+                        currentToken = PicatTokenTypes.HEX_INTEGER
+                        return
+                    }
+                    'o', 'O' -> {
+                        tokenEnd = skipOctalNumber(tokenStart)
+                        currentToken = PicatTokenTypes.OCTAL_INTEGER
+                        return
+                    }
+                    'b', 'B' -> {
+                        tokenEnd = skipBinaryNumber(tokenStart)
+                        currentToken = PicatTokenTypes.BINARY_INTEGER
+                        return
+                    }
+                }
+            }
+
             tokenEnd = skipNumber(tokenStart)
             currentToken = if (buffer.substring(tokenStart, tokenEnd).contains('.')) {
                 PicatTokenTypes.FLOAT
@@ -104,6 +132,7 @@ class PicatLexer : LexerBase() {
                 "index" -> PicatTokenTypes.INDEX_KEYWORD
                 "private" -> PicatTokenTypes.PRIVATE_KEYWORD
                 "public" -> PicatTokenTypes.PUBLIC_KEYWORD
+                "table" -> PicatTokenTypes.TABLE_KEYWORD
                 "end" -> PicatTokenTypes.END_KEYWORD
                 "if" -> PicatTokenTypes.IF_KEYWORD
                 "then" -> PicatTokenTypes.THEN_KEYWORD
@@ -117,7 +146,13 @@ class PicatLexer : LexerBase() {
                 "throw" -> PicatTokenTypes.THROW_KEYWORD
                 "try" -> PicatTokenTypes.TRY_KEYWORD
                 "catch" -> PicatTokenTypes.CATCH_KEYWORD
-                "mod" -> PicatTokenTypes.MODULO
+                "not" -> PicatTokenTypes.NOT_KEYWORD
+                "once" -> PicatTokenTypes.ONCE_KEYWORD
+                "div" -> PicatTokenTypes.DIV_KEYWORD
+                "mod" -> PicatTokenTypes.MOD_KEYWORD
+                "rem" -> PicatTokenTypes.REM_KEYWORD
+                "in" -> PicatTokenTypes.IN_KEYWORD
+                "notin" -> PicatTokenTypes.NOTIN_KEYWORD
                 else -> PicatTokenTypes.IDENTIFIER
             }
             return
@@ -126,33 +161,236 @@ class PicatLexer : LexerBase() {
         // Handle operators and separators
         tokenEnd = tokenStart + 1
 
+        // Check for four-character operators
+        if (tokenStart + 3 < bufferEnd) {
+            val fourChars = buffer.substring(tokenStart, tokenStart + 4)
+            when (fourChars) {
+                "#<=>" -> {
+                    tokenEnd = tokenStart + 4
+                    currentToken = PicatTokenTypes.CONSTRAINT_EQUIV
+                    return
+                }
+            }
+        }
+
         // Check for three-character operators
-        if (tokenEnd + 1 < bufferEnd) {
+        if (tokenStart + 2 < bufferEnd) {
             val threeChars = buffer.substring(tokenStart, tokenStart + 3)
-            if (threeChars == "?=>") {
-                tokenEnd = tokenStart + 3
-                currentToken = PicatTokenTypes.BACKTRACKABLE_ARROW_OP
-                return
+            when (threeChars) {
+                "?=>" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.BACKTRACKABLE_ARROW_OP
+                    return
+                }
+                "!==" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.NOT_IDENTICAL
+                    return
+                }
+                "===" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.IDENTICAL
+                    return
+                }
+                "#=>" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_IMPL
+                    return
+                }
+                "#/\\" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_AND
+                    return
+                }
+                "#\\/" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_OR
+                    return
+                }
+                "#<=" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_LE_ALT
+                    return
+                }
+                "#!=" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_NEQ
+                    return
+                }
+                "#>=" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_GE
+                    return
+                }
+                "#=<" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.CONSTRAINT_LE
+                    return
+                }
+                "@=<" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.TERM_LE
+                    return
+                }
+                "@<=" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.TERM_LE_ALT
+                    return
+                }
+                "@>=" -> {
+                    tokenEnd = tokenStart + 3
+                    currentToken = PicatTokenTypes.TERM_GE
+                    return
+                }
             }
         }
 
         // Check for two-character operators
-        if (tokenEnd < bufferEnd) {
+        if (tokenStart + 1 < bufferEnd) {
             val twoChars = buffer.substring(tokenStart, tokenStart + 2)
-            if (twoChars == "=>" || twoChars == "==" || twoChars == "!=" || 
-                twoChars == "<=" || twoChars == ">=" || twoChars == "&&" || twoChars == "||") {
-                tokenEnd = tokenStart + 2
-                currentToken = when (twoChars) {
-                    "=>" -> PicatTokenTypes.ARROW_OP
-                    "==" -> PicatTokenTypes.EQUAL
-                    "!=" -> PicatTokenTypes.NOT_EQUAL
-                    "<=" -> PicatTokenTypes.LESS_EQUAL
-                    ">=" -> PicatTokenTypes.GREATER_EQUAL
-                    "&&" -> PicatTokenTypes.AND
-                    "||" -> PicatTokenTypes.OR
-                    else -> PicatTokenTypes.BAD_CHARACTER
+            when (twoChars) {
+                "=>" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.ARROW_OP
+                    return
                 }
-                return
+                "==" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.EQUAL
+                    return
+                }
+                "!=" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.NOT_EQUAL
+                    return
+                }
+                "<=" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.LESS_EQUAL
+                    return
+                }
+                "=<" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.LESS_EQUAL_ALT
+                    return
+                }
+                ">=" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.GREATER_EQUAL
+                    return
+                }
+                "&&" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.AND
+                    return
+                }
+                "||" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.OR
+                    return
+                }
+                "**" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.POWER_OP
+                    return
+                }
+                "//" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.INT_DIVIDE
+                    return
+                }
+                "/<" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.DIVIDE_LT
+                    return
+                }
+                "/>" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.DIVIDE_GT
+                    return
+                }
+                ":=" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.ASSIGN_ONCE
+                    return
+                }
+                "::" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.TYPE_CONSTRAINT
+                    return
+                }
+                ".." -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.RANGE
+                    return
+                }
+                "++" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.CONCAT
+                    return
+                }
+                "<<" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.SHIFT_LEFT
+                    return
+                }
+                ">>" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.SHIFT_RIGHT
+                    return
+                }
+                "/\\" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.BITWISE_AND
+                    return
+                }
+                "\\/" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.BITWISE_OR
+                    return
+                }
+                "#=" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.CONSTRAINT_EQ
+                    return
+                }
+                "#!" -> {
+                    // Skip this case, as "#!=" is handled by three-character operators
+                    // and "#!" by itself is not a valid operator
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.BAD_CHARACTER
+                    return
+                }
+                "#<" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.CONSTRAINT_LT
+                    return
+                }
+                "#>" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.CONSTRAINT_GT
+                    return
+                }
+                "#~" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.CONSTRAINT_NOT
+                    return
+                }
+                "#^" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.CONSTRAINT_XOR
+                    return
+                }
+                "@<" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.TERM_LT
+                    return
+                }
+                "@>" -> {
+                    tokenEnd = tokenStart + 2
+                    currentToken = PicatTokenTypes.TERM_GT
+                    return
+                }
             }
         }
 
@@ -225,6 +463,17 @@ class PicatLexer : LexerBase() {
         return i
     }
 
+    private fun skipQuotedAtom(start: Int): Int {
+        var i = start + 1
+        while (i < bufferEnd) {
+            if (buffer[i] == '\'' && buffer[i - 1] != '\\') {
+                return i + 1
+            }
+            i++
+        }
+        return i
+    }
+
     private fun skipNumber(start: Int): Int {
         var i = start
         var hasDecimalPoint = false
@@ -235,6 +484,21 @@ class PicatLexer : LexerBase() {
                 i++
             } else if (isDigit(buffer[i])) {
                 i++
+            } else if (buffer[i] == '_') {
+                // Allow underscores in numbers for readability (e.g., 1_000_000)
+                i++
+            } else if (i + 1 < bufferEnd && (buffer[i] == 'e' || buffer[i] == 'E') && 
+                      (isDigit(buffer[i + 1]) || buffer[i + 1] == '+' || buffer[i + 1] == '-')) {
+                // Handle exponent notation (e.g., 1.0e10, 1.0e+10, 1.0e-10)
+                i++
+                if (buffer[i] == '+' || buffer[i] == '-') {
+                    i++
+                }
+                // Skip the digits in the exponent
+                while (i < bufferEnd && isDigit(buffer[i])) {
+                    i++
+                }
+                break
             } else {
                 break
             }
@@ -242,6 +506,64 @@ class PicatLexer : LexerBase() {
 
         return i
     }
+
+    private fun skipHexNumber(start: Int): Int {
+        // Skip the '0x' or '0X' prefix
+        var i = start + 2
+
+        while (i < bufferEnd) {
+            if (isHexDigit(buffer[i])) {
+                i++
+            } else if (buffer[i] == '_') {
+                // Allow underscores in numbers for readability
+                i++
+            } else {
+                break
+            }
+        }
+
+        return i
+    }
+
+    private fun skipOctalNumber(start: Int): Int {
+        // Skip the '0o' or '0O' prefix
+        var i = start + 2
+
+        while (i < bufferEnd) {
+            if (isOctalDigit(buffer[i])) {
+                i++
+            } else if (buffer[i] == '_') {
+                // Allow underscores in numbers for readability
+                i++
+            } else {
+                break
+            }
+        }
+
+        return i
+    }
+
+    private fun skipBinaryNumber(start: Int): Int {
+        // Skip the '0b' or '0B' prefix
+        var i = start + 2
+
+        while (i < bufferEnd) {
+            if (isBinaryDigit(buffer[i])) {
+                i++
+            } else if (buffer[i] == '_') {
+                // Allow underscores in numbers for readability
+                i++
+            } else {
+                break
+            }
+        }
+
+        return i
+    }
+
+    private fun isHexDigit(c: Char): Boolean = c in '0'..'9' || c in 'a'..'f' || c in 'A'..'F'
+    private fun isOctalDigit(c: Char): Boolean = c in '0'..'7'
+    private fun isBinaryDigit(c: Char): Boolean = c == '0' || c == '1'
 
     private fun skipIdentifier(start: Int): Int {
         var i = start
