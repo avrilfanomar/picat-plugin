e01e51aec3b6307b320fc460817a5bfe4a221d5c
Andrii Andriichuk <andrey000mar@gmail.com>
2025-07-15 19:48:13 +0200

    build.gradle.kts cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
@@ -70,7 +69,7 @@ intellijPlatform {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n")
             }
         }
 
